e24c8f3806f013ad49dd61f88f863c9c645ee0db
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-12 21:00:00 +0530

    Child Class

---
+++ Android.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Android : Mobile
+{
+    string osVersion;
+    public Android(string brand, string model, string osVersion) : base(brand, model)
+    {
+        this.osVersion = osVersion;
+    }
+
+    public void PrintPhoneDetails()
+    {
+        base.PrintDetails();
+        Console.WriteLine("Current Version: " + osVersion);
+    }
+}
