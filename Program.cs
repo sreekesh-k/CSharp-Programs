f93d8ad86829bf70a1a06b3e76d9ede136e022fb
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-12 21:01:51 +0530

    Main function

--- Program.cs
+++ Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+public class Program
+{
+    public Program()
+    {
+    }
+    static void Main(string[] args)
+    {
+        Android myMobile = new Android("IQoo", "Z9", "14o");
+        myMobile.ChargeBattery(75);
+        myMobile.MakeCall(100);
+        myMobile.TakePhoto();
+        myMobile.GetLocation();
+        myMobile.PrintPhoneDetails();
+    }
+}
