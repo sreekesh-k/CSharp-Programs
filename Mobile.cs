b16b4675313df149c38c62821254759622abc488
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-12 20:58:13 +0530

    Initial Commit

---
+++ Mobile.cs
@@ -0,0 +1,32 @@
+﻿using System;
+/*
+	1. Wap to create a class Mobile, the class should have following attributes and methods:
+		Fields:-
+			1. brand
+			2. model
+			3. batteryLevel
+
+		Methods:-
+			1. MakeCall()
+			2. ChargeBattery(battery)
+			3. UseBattery(battery)
+			4. PrintDetails()
+
+	2. After creating the class WAP to create and instance of Mobile class set the attributes brand and model.
+	3. Call the ChargeBattery() to set battery level to 50%.
+	4. Call the MakeCall() to simulate making a call.
+	5. Call the USeBattery() to simulate battery usage.
+	6. Print the details of mobile using PrintDetails().
+	7. Create a sub class Android use the methods and properties inside the base class.
+	8. Android has OS and Version.
+	9. Create 2 interfaces:
+			1. Icamera - TakePhoto()
+			2. Igps - GetLocation()
+		The android class should implement both these interfaces.
+ */
+public class Mobile
+{
+	public class Mobile() { }
+
+}
+
