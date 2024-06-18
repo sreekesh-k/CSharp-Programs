1bd7c3cbdcff0637806da7fde49fd29a74bdef1a
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-18 19:41:35 +0530

    Delegate Demo

---
+++ DelegateDemo/MathOperaation.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+delegate int CalculateValue(int x, int y);
+public class MathOperaation
+{
+	public MathOperaation()
+	{
+	}
+
+	public static int Sum(int a, int b)
+	{
+		return a + b;
+	}
+	public static int Sub(int a, int b)
+	{
+		return a - b;
+	}
+
+}
--- DelegateDemo/Program.cs
+++ DelegateDemo/Program.cs
@@ -4,8 +4,21 @@ public class Program
 {
     public Program() { }
 
-    static void Main()
+    static void Main(string []args)
     {
-        Console.WriteLine("Hello World");
+        CalculateValue cv1 = new CalculateValue(MathOperaation.Sum);
+        CalculateValue cv2 = new CalculateValue(MathOperaation.Sub);
+
+        Console.WriteLine(cv1(100,200));
+        Console.WriteLine(cv2(200,100));
+
+        CalculateValue cv3 = cv1 + cv2;
+        CalculateValue cv4 = cv3 - cv2;
+
+
+        Console.WriteLine(cv3(100, 200));
+        Console.WriteLine(cv4(100, 200));
+
     }
+        
 }
