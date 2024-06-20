20adf3106a4babea35560e4a0dfaae70a24f09f4
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-20 10:10:04 +0530

    GenericClass Demo

---
+++ GenericClassDemo/GCDemocs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+public class GCDemo <T>
+{
+	T a, b;
+	public GCDemo(T a, T b )
+	{
+		this.a = a;
+		this.b = b;	
+	}
+
+	public void Add()
+	{
+        Console.WriteLine($"Type of a: {a.GetType()}");
+        Console.WriteLine($"Type of b: {b.GetType()}");
+    }
+
+
+
+}
---
+++ GenericClassDemo/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+
+
+class Program
+{
+    public Program() { }
+
+    static void Main(string[] args)
+    {
+        GCDemo<int> myInt = new GCDemo<int>(1,2);
+        myInt.Add();
+        GCDemo<string> myString = new GCDemo<string>("Hello"," World");
+        myString.Add();
+    }
+}
