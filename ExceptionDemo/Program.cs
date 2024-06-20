6cffb4a78cf3b8cdcc092dcff0045ed70f23db08
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-20 09:36:56 +0530

    Exception Demo

---
+++ ExceptionDemo/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+
+class Program
+{
+    public Program() { }
+
+    public static void Main(string[] args)
+    {
+        objectsDemo.ObjectDemo();
+        /*int[] arr = { 1, 2, 3, 4, 5 };
+        try
+        {
+            Console.WriteLine(arr[5]);
+
+        }
+        catch (IndexOutOfRangeException ex)
+        {
+            Console.WriteLine(ex.ToString());
+        }*/
+    }
+}
---
+++ ExceptionDemo/objectDemo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+public class objectsDemo
+{
+    public objectsDemo() { }
+
+    public static void ObjectDemo()
+    {
+        try
+        {
+            string[] stringArray = new string[5];
+            stringArray[0] = "Hello, World!";
+            // Attempt to assign an integer to the string array
+            // This will throw an ArrayTypeMismatchException
+            object[] objectArray = stringArray;
+            objectArray[1] = 42; // This is invalid and will cause an exception
+
+            Console.WriteLine("No Error");
+        }
+        catch (ArrayTypeMismatchException ae)
+        {
+            Console.WriteLine("ArrayTypeMismatchException caught!");
+            Console.WriteLine(ae.ToString());
+        }
+    }
+}
