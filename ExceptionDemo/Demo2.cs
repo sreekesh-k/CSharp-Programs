17f3b03a22da79f09cb08de2e684eb3ad97c53b0
Sreekesh-k <sreekeshkprabhu.arp@gmail.com>
2024-06-20 09:53:57 +0530

    Demo2

---
+++ ExceptionDemo/Demo2.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+	public void Test()
+	{
+		object []myObj = new object[2];
+		myObj[0] = new object[1];
+        ((object[])myObj[0])[0] = "HELLO"; 
+        Console.WriteLine(((object[])myObj[0])[0]);
+    }
+}
